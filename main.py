64e9f16146a7b83d0013f7e764e7f826f6c522e7
GabrielGelgor <gabriel.gelgor@uoit.net>
2020-02-22 17:02:35 -0500

    Randominit + data parsing complete

--- main.py
+++ main.py
@@ -7,19 +7,35 @@ K
 e
     e - the number of epochs to run for
 '''
+import random as rand
 
 #Function to select random starting points based off of the training examples. 
 #Options for running multiple times is provided as a way to break out of local optima.
 def randomInit(data, K, iterations=1):
     #while we have iterations left:
+    centroids = []
+    for iters in range(iterations):
+        p_mu = []
         #while we haven't picked K centroids:
+        while (len(p_mu) < K):
             #Pick a record index within the range of the data set
+            attempt = rand.randint(0, len(data))
             #Have we already picked this index?
-            #No:
-                #Store this number
+            if (len(p_mu) == 0):
+                p_mu.append(data[attempt])
+                continue
+
+            for i in range(len(p_mu)):
+                if (p_mu[i] == data[attempt]):
+                    break
+                
+                #No: Store this number
+                if (i == len(p_mu)-1):
+                    p_mu.append(data[attempt])
 
         #add these centroid starting points to the output set of centroid starting points.
-    pass
+        centroids.append(p_mu[:])
+    return centroids
 
 def kmeans(data, centroids, epochs):
     #for every epoch of running:
@@ -46,4 +62,10 @@ Cluster array - A 2D array containing K clusters of training examples.
 J - Distortion value for this run of k-means.
 '''
 
-#TODO: Parse incoming data.
+data = []
+
+with open("dataset_simple.txt") as file:
+    for line in file:
+        string = line[0:-2]
+        data.append([float(i) for i in string.split(',')])
+
